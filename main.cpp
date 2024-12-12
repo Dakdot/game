a8de58843de87825db24d74545abdb8750870f83
Thiago Andrade <tja.usa.202@gmail.com>
2024-12-12 16:29:47 -0500

    A LOT OF CHANGES

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
+// TODO:
+// - Implement winning / losing
+// - Implement say UNO & failed to say UNO
+// If we have time:
+// - Bot difficulty
+
 #include <cstddef>
 #include <cstdio>
 #include <cstdlib>
+#include <ctime>
 #include <iostream>
 #include <stdexcept>
+#include <string>
 #include <thread>
 #include <vector>
 
@@ -31,10 +39,15 @@ using namespace std::chrono_literals;
 #define WILD_CARD 0xdd
 #define WILD_DRAW_FOUR 0xee
 
+// Control how many times the `shuffle()` function randomly swaps cards
+#define SHUFFLE_RANDOM_PASSES 1000
+
 std::vector<unsigned char> deck;
 std::vector<unsigned char> discard;
 std::vector<unsigned char> player_hand;
 std::vector<unsigned char> cpu_hand;
+
+// CARD HELPER FUNCTIONS
 
 void move_card(std::vector<unsigned char> &from,
                std::vector<unsigned char> &to) {
@@ -45,18 +58,23 @@ void move_card(std::vector<unsigned char> &from,
   to.push_back(c);
 }
 
-void deal_cards(std::vector<unsigned char> &from,
-                std::vector<unsigned char> &to) {
-  for (int i = 0; i < 7; i++) {
-    move_card(from, to);
+void play_card(std::vector<unsigned char> &from, std::vector<unsigned char> &to,
+               unsigned char c) {
+  for (int i = 0; i < from.size(); i++) {
+    if (from[i] == c) {
+      from.erase(from.begin() + i);
+      to.push_back(c);
+    }
   }
 }
 
+// Shuffle cards by randomly selecting two cards and swapping their position
+// in the vector.
 void shuffle(std::vector<unsigned char> &deck) {
   srand(time(0));
   size_t deck_size = deck.size();
 
-  for (int i = 0; i < 1000; i++) {
+  for (int i = 0; i < SHUFFLE_RANDOM_PASSES; i++) {
     unsigned int idx_first = rand() % deck_size;
     unsigned int idx_second = rand() % deck_size;
 
@@ -100,17 +118,8 @@ void print_deck(std::vector<unsigned char> &deck) {
   printf("\n%lu elements\n", deck.size());
 }
 
-void print_card(unsigned char c) {
-  unsigned char number_nib = c & 0x0f;
+void print_color(unsigned char c) {
   unsigned char color_nib = c & 0xf0;
-
-  if (c == WILD_CARD) {
-    printf("\x1b[;95mWILD CARD");
-  } else if (c == WILD_DRAW_FOUR) {
-    return;
-    printf("\x1b[;95mWILD DRAW FOUR");
-    return;
-  }
 
   if (color_nib == RED)
     printf("\x1b[;31mRED");
@@ -120,6 +129,23 @@ void print_card(unsigned char c) {
     printf("\x1b[;32mGREEN");
   else if (color_nib == BLUE)
     printf("\x1b[;34mBLUE");
+
+  if ((c & 0x0f) != 0x0f)
+    printf("\x1b[;0m");
+}
+
+void print_card(unsigned char c) {
+  unsigned char number_nib = c & 0x0f;
+
+  if (c == WILD_CARD) {
+    printf("\x1b[;95mWILD CARD");
+    return;
+  } else if (c == WILD_DRAW_FOUR) {
+    printf("\x1b[;95mWILD DRAW FOUR");
+    return;
+  }
+
+  print_color(c | 0x0f);
 
   if (number_nib <= 9) {
     printf(" %d", number_nib);
@@ -145,16 +171,187 @@ void print_hand(std::vector<unsigned char> &deck) {
   printf("\n");
 }
 
+unsigned char prompt_color(const std::string &prompt) {
+  std::cout << prompt << std::endl;
+  std::cout << "\x1b[;31mr\x1b[;33my\x1b[;32mg\x1b[;34mb\x1b[;0m > ";
+  std::string line;
+  std::getline(std::cin, line);
+
+  if (line == "r") {
+    return RED;
+  } else if (line == "y") {
+    return YELLOW;
+  } else if (line == "g") {
+    return GREEN;
+  } else if (line == "b") {
+    return BLUE;
+  }
+}
+
+// MAIN GAME LOGIC
+
+char turn = 0;      // 0 = player, 1 = cpu
+char direction = 1; // 1 or -1
+
+void deal_cards(std::vector<unsigned char> &from,
+                std::vector<unsigned char> &to) {
+  for (int i = 0; i < 7; i++) {
+    move_card(from, to);
+  }
+}
+
 void flip_first_card() {
   std::cout << "The first card is flipped..." << std::endl;
   move_card(deck, discard);
   std::this_thread::sleep_for(2s);
   std::cout << "It's a ";
   print_card(discard.back());
-  std::cout << "!";
+  std::cout << "!" << std::endl;
 }
 
-int main() {
+void process_card(unsigned char &c) {
+  srand(time(0));
+
+  if (c == WILD_CARD || c == WILD_DRAW_FOUR) {
+    unsigned char col;
+    if (turn == 0) {
+      col = prompt_color("You get to choose the color!");
+    } else {
+      unsigned char col = rand() & 0x03;
+    }
+    c = (c & 0x0f) | col;
+    std::cout << "The color in play is now ";
+    print_color(col);
+    std::cout << "!" << std::endl;
+
+    if (c == WILD_DRAW_FOUR) {
+      if (turn == 0) {
+        std::cout << "CPU draws four cards!" << std::endl;
+        for (int i = 0; i < 4; i++) {
+          move_card(deck, cpu_hand);
+        }
+      } else {
+        std::cout << "You draw four cards!" << std::endl;
+        for (int i = 0; i < 4; i++) {
+          move_card(deck, player_hand);
+        }
+      }
+    }
+  } else if ((c & 0x0f) == SKIP) {
+    if (turn == 0) {
+      std::cout << "CPU was skipped!" << std::endl;
+    } else {
+      std::cout << "You were skipped!" << std::endl;
+    }
+  } else if ((c & 0x0f) == REVERSE) {
+    if (turn == 0) {
+
+    } else {
+    }
+  } else if ((c & 0x0f) == DRAW_TWO) {
+    if (turn == 0) {
+      std::cout << "CPU drew two cards! Its turn was skipped." << std::endl;
+      move_card(deck, cpu_hand);
+      move_card(deck, cpu_hand);
+    } else {
+      std::cout << "You drew two cards!" << std::endl;
+      move_card(deck, player_hand);
+      move_card(deck, player_hand);
+    }
+  } else {
+    if (turn == 0)
+      turn = 1;
+    else
+      turn = 0;
+  }
+}
+
+void process_turn() {
+  unsigned char top = discard.back();
+  srand(time(0));
+
+  if (turn == 0) {
+    std::cout << "It is your turn. Your hand is:" << std::endl;
+    print_hand(player_hand);
+
+    std::vector<unsigned char> playables;
+    for (auto c : player_hand) {
+      if ((c & 0x0f) == (top & 0x0f) || (c & 0xf0) == (top & 0xf0)) {
+        playables.push_back(c);
+      }
+    }
+
+    if (playables.empty()) {
+      std::cout << "You have no playable cards! Press RETURN to draw one..."
+                << std::endl;
+      std::string unused;
+      std::getline(std::cin, unused);
+
+      move_card(deck, player_hand);
+      unsigned char drawn = player_hand.back();
+      std::cout << "You drew a ";
+      print_card(drawn);
+      std::cout << ". ";
+      if ((drawn & 0x0f) == (top & 0x0f) || (drawn & 0xf0) == (top & 0xf0)) {
+        std::cout << "Press RETURN to play it." << std::endl;
+        std::getline(std::cin, unused);
+        move_card(player_hand, discard);
+      } else {
+        std::cout << "Your turn is over." << std::endl;
+      }
+
+    } else {
+      std::cout << "Please type the index of the card you would like to play: "
+                << std::endl;
+      for (int i = 0; i < playables.size(); i++) {
+        std::cout << "[" << i << "] ";
+        print_card(playables[i]);
+        std::cout << std::endl;
+      }
+
+      int result;
+      do {
+        std::cout << "> ";
+        std::cin >> result;
+      } while (result > playables.size() || result < 0);
+      play_card(player_hand, discard, playables[result]);
+    }
+  }
+
+  if (turn == 1) {
+    std::vector<unsigned char> playables;
+    for (auto c : cpu_hand) {
+      if ((c & 0x0f) == (top & 0x0f) || (c & 0xf0) == (top & 0xf0)) {
+        playables.push_back(c);
+      }
+    }
+
+    if (playables.empty()) {
+      std::cout << "CPU drew a card!";
+      move_card(deck, cpu_hand);
+
+      unsigned char drawn = cpu_hand.back();
+      if ((drawn & 0x0f) == (top & 0x0f) || (drawn & 0xf0) == (top & 0xf0)) {
+        play_card(cpu_hand, discard, drawn);
+        std::cout << std::endl << "CPU placed a ";
+        print_card(drawn);
+        std::cout << "!" << std::endl;
+      } else {
+        std::cout << " That was the end of its turn." << std::endl;
+      }
+    } else {
+      unsigned char idx = rand() % playables.size();
+      unsigned char c = playables[idx];
+
+      play_card(cpu_hand, discard, c);
+      std::cout << "CPU played a ";
+      print_card(c);
+      std::cout << "!" << std::endl;
+    }
+  }
+}
+
+void setup() {
   std::cout << "Welcome to UNO!" << std::endl;
   std::cout << "You and your opponent are each dealt seven cards..."
             << std::endl;
@@ -166,12 +363,18 @@ int main() {
 
   std::this_thread::sleep_for(3s);
 
-  std::cout << "Your hand:" << std::endl;
-  print_hand(player_hand);
-
-  std::this_thread::sleep_for(3s);
-
   flip_first_card();
+}
+
+int main() {
+  setup();
+
+  while (true) {
+    unsigned char &top = discard.back();
+    process_card(top);
+    process_turn();
+    std::this_thread::sleep_for(5s);
+  }
 
   return 0;
 }
